81b84f241caf5f79057b00674c6524f24aceadfb
Shckipper <savelmilo337@gmail.com>
2024-11-25 18:17:52 +0900

    Добавьте файлы проекта.

---
+++ Dll/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleLibrary;
+
+
+namespace Dll
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                SimpleFunctions simpleFunctions = new SimpleFunctions();
+
+                // Метод 1: Приветствие
+                simpleFunctions.PrintGreeting();
+
+                // Метод 2: Текст в рамке
+                simpleFunctions.PrintBox("Hello, Simple Library!");
+
+                // Метод 3: Счётчик слов 
+                simpleFunctions.CountWordsFromUser();
+
+                // Метод 4: Генерация случайной строки
+                simpleFunctions.GenerateRandomString(10);
+
+                // Метод 5: Переворот строки 
+                simpleFunctions.ReverseUserString();
+                Console.ReadKey();
+
+            }
+        }
+}
+
+
---
+++ Dll/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Dll")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dll")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d8eabe25-29be-48b1-aec5-02633f06a43a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
